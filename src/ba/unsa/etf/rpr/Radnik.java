07a46d09cba7a9ba372e44abdaee223a54939fa2
Tarik <tsijercic1@etf.unsa.ba>
2019-11-18 15:37:28 +0100

    sve se kompajlira

---
+++ src/ba/unsa/etf/rpr/Radnik.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rpr;
+
+public class Radnik {
+    private String imePrezime, jmbg;
+
+    public Radnik(String imePrezime, String jmbg) {
+        this.imePrezime = imePrezime;
+        this.jmbg = jmbg;
+    }
+
+    public double prosjecnaPlata() {
+        return 0;
+    }
+
+    public String getImePrezime() {
+        return null;
+    }
+
+    public String getJmbg() {
+        return null;
+    }
+
+    public void setImePrezime(String pero_perić) {
+
+    }
+
+    public void setJmbg(String s) {
+
+    }
+
+    public void dodajPlatu(int i) {
+
+    }
+}
---
+++ src/ba/unsa/etf/rpr/RadnoMjesto.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr;
+
+public class RadnoMjesto {
+
+    public RadnoMjesto(String naziv, double koeficijent, Radnik radnik) {
+
+    }
+
+    public RadnoMjesto() {
+
+    }
+
+    public String getNaziv() {
+        return null;
+    }
+
+    public int getKoeficijent() {
+        return 0;
+    }
+
+    public Radnik getRadnik() {
+            return null;
+    }
+
+    public void setNaziv(String portir) {
+
+    }
+
+    public void setKoeficijent(int i) {
+
+    }
+}
